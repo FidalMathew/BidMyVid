0cf785db593798808ee831cc2ea29f21431ac840
Fidal <fidal15perfect@gmail.com>
2023-06-10 16:25:31 +0530

    add backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.post('/api/check-access', (req, res) => {
+
+    const { accessKey, context, timeStamp } = req.body;
+    const assetId = context.assetId;
+    const userId = context.userId;
+
+    res.status(200).send('Data received successfully.');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
--- client/src/pages/Livepeer.jsx
+++ client/src/pages/Livepeer.jsx
@@ -40,3 +40,11 @@ export function Livepeer() {
         />
     );
 }
+
+// import { Player } from '@livepeer/react';
+
+export const CreateAndViewAsset = () => {
+    const accessKey = getAccessKeyForYourApplication();
+
+    return <Player playbackId={playbackId} accessKey={accessKey} />;
+};
