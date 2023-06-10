46f6937c79b70cc26c14a7d56e40d5a1636260a5
Fidal <fidal15perfect@gmail.com>
2023-06-10 17:23:42 +0530

    added morgan, cors, json

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+const morgan = require('morgan');
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
