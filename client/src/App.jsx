5eb40d3e662292f79062b308d438128063452473
Fidal <fidal15perfect@gmail.com>
2023-06-10 09:15:13 +0530

    initialize livepeer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import {
+  LivepeerConfig,
+  // ThemeConfig,
+  createReactClient,
+  studioProvider,
+} from '@livepeer/react';
+import { Livepeer } from './pages/Livepeer';
+
+const VITE_LIVEPEER_STUDIO_API_KEY = import.meta.env.VITE_LIVEPEER_STUDIO_API_KEY;
+
+console.log('VITE_LIVEPEER_STUDIO_API_KEY', VITE_LIVEPEER_STUDIO_API_KEY);
+
+const client = createReactClient({
+  provider: studioProvider({ apiKey: VITE_LIVEPEER_STUDIO_API_KEY }),
+});
+
+const livepeerTheme = {
+  colors: {
+    accent: 'rgb(0, 145, 255)',
+    containerBorderColor: 'rgba(0, 145, 255, 0.9)',
+  },
+  fonts: {
+    display: 'Inter',
+  },
+};
+
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <LivepeerConfig client={client} theme={livepeerTheme}>
+      <Livepeer />
+    </LivepeerConfig>
+  );
 }
-
 export default App
---
+++ client/src/pages/Livepeer.jsx
@@ -0,0 +1,42 @@
+import { Player } from '@livepeer/react';
+// import * as React from 'react';
+import pic from '../assets/react.svg';
+
+// import waterfallsPoster from '../../public/images/waterfalls-poster.png';
+
+const PosterImage = () => {
+    // return (
+    //     <Image
+    //         src={pic}
+    //         layout="fill"
+    //         objectFit="cover"
+    //         priority
+    //         placeholder="blur"
+    //     />
+    // );
+
+    return (
+        <img
+            src={pic}
+            alt="Picture of the author"
+
+            placeholder="blur"
+        />)
+};
+
+const playbackId =
+    'bafybeigtqixg4ywcem3p6sitz55wy6xvnr565s6kuwhznpwjices3mmxoe';
+
+export function Livepeer() {
+    return (
+        <Player
+            title="Waterfalls"
+            playbackId={playbackId}
+            loop
+            autoPlay
+            showTitle={false}
+            muted
+            poster={<PosterImage />}
+        />
+    );
+}
