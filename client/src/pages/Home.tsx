1963beeacd59a8e926c484916c8cdb67ce91a5d4
ayuarya <ayu.arya72@gmail.com>
2023-06-16 17:10:47 +0530

    added cards props

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,28 +7,56 @@ import * as React from 'react'
 import { Contract } from "../initializers/ethers"
 
 const Homecomponent = () => {
+    const convertDateAndTime = (timestamp) => {
+        const decimalTimestamp = parseInt(timestamp.substring(2), 16);
+        const date = new Date(decimalTimestamp * 1000);
+        const year = date.getFullYear();
+        const month = ('0' + (date.getMonth() + 1)).slice(-2);
+        const day = ('0' + date.getDate()).slice(-2);
+        const hours = ('0' + date.getHours()).slice(-2);
+        const minutes = ('0' + date.getMinutes()).slice(-2);
+        const seconds = ('0' + date.getSeconds()).slice(-2);
 
+        const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+        return formattedDateTime;
+    };
+    const[auctionitems,setAuctionItems]=React.useState([])
     React.useEffect(() => {
         const getAllAuctions = async () => {
+            let arr:any=[];
             const res = await Contract.getAllAuctions()
-            console.log(res)
+            res.map((item:any)=>{
+                let values={
+                    biddable:item.biddable,
+                    bids:Number(item.bids._hex),
+                    description:item.description,
+                    endTime:convertDateAndTime(item.endTime._hex),
+                    image:item.image,
+                    live:item.live,
+                    name:item.name,
+                    owner:item.owner,
+                    price:Number(item.price),
+                    sold:item.sold,
+                    tokenId:Number(item.tokenId),
+                    winner:item.winner,
+                }
+                arr.push(values);
+            })
+            console.log(arr)
+            setAuctionItems(arr)
         }
         getAllAuctions()
     }, [Contract])
-
     return (
         <>
             <Navbar />
             <Box>
                 <Text fontSize="4xl" textAlign="center" fontWeight="bold" mt="10"></Text>
                 <VStack>
-                    <Cards />
-                    <Cards />
-                    <Cards />
-                    <Cards />
-                    <Cards />
-                    <Cards />
-                    <Cards />
+                    {auctionitems.map((item:any,index:any)=>{
+                        return  <Cards auctionItem={item} key={index}/>
+                    })}
                 </VStack>
             </Box>
         </>
